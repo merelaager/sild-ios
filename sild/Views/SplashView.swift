44226ec1513e5e80a61dd2418322be48df56f747
Taka <71265675+takakv@users.noreply.github.com>
2026-06-27 02:11:23 +0300

    Align splashview with launchscreen

--- sild/Views/SplashView.swift
+++ sild/Views/SplashView.swift
@@ -9,15 +9,15 @@ struct SplashView: View {
     private let background = Color(red: 35.0 / 255.0, green: 64.0 / 255.0, blue: 143.0 / 255.0)
 
     var body: some View {
-        background
-            .ignoresSafeArea()
-            .overlay {
-                Image("ship_w")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 144)
-                    .foregroundStyle(.white)
-            }
+        ZStack {
+            background
+            Image("ship_w")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 144)
+                .foregroundStyle(.white)
+        }
+        .ignoresSafeArea()
     }
 }
 
